4c79442dfd041e53e2a612ee86dc208bf0d433a5
Andy Hieb <andy@dtek.net>
2014-06-11 07:49:00 -0700

    [#7] Stub in some pseudo-code for standings.

--- app/scripts/controllers/standings.js
+++ app/scripts/controllers/standings.js
@@ -4,5 +4,15 @@ angular.module('wbwcpNgApp')
   .controller('StandingsCtrl', function ($scope, User, Auth) {
     $scope.errors = {};
 
+    // Find highest match number where we have a result recorded.
     
+    // Populate select box w/ integers equal to that match number and lower.
+    
+    // For selected match number and below (up to 48), retrieve points for all users into array, sorted by points desc.
+    
+    // Above 48, build two separate arrays: one for KO round, and one for "total" (first round + KO)
+    
+    // View prints table(s) of username and points totals.
+
+
   });
