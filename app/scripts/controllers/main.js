21955c5a33fb609d627a8c6759abd2889f3a8895
Andy Hieb <andy@dtek.net>
2014-06-22 20:42:30 -0700

    Fix links to hash fragments.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,15 @@
 'use strict';
 
 angular.module('wbwcpNgApp')
-  .controller('MainCtrl', function ($scope, $http, Auth) {
+  .controller('MainCtrl', function ($scope, $http, $location, $anchorScroll, Auth) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
       $scope.isAdmin = (Auth.isLoggedIn() && Auth.isAdmin()) ? true : false;
     });
+
+    $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+    }
+
   });
